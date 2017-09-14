d76d5ab40521e403aed9234450cd1c71e3e2e22b
hongmingu <gmh397@nate.com>
2017-09-15 01:03:56 +0900

    second

--- laymeibigmsg/models.py
+++ laymeibigmsg/models.py
@@ -4,3 +4,23 @@ from django.db import models
 class BigMsg(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+
+
+class BigMsgFromUser(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class BigMsgText(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class BigMsgToUser(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class BigMsgImage(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
--- laymeismallmsg/models.py
+++ laymeismallmsg/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class SmallMsg(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class SmallMsgText(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class SmallMsgFromTo(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class SmallMsgBigMsg(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
