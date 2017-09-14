b835d58f76c1f6cfeb5d43fc221454fe4302a2b0
hongmingu <gmh397@nate.com>
2017-09-14 22:41:51 +0900

    Initial commit

---
+++ laymeismallmsg/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LaymeismallmsgConfig(AppConfig):
+    name = 'laymeismallmsg'
---
+++ laymeiuser/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LaymeiuserConfig(AppConfig):
+    name = 'laymeiuser'
